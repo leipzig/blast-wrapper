76325d0cdc6c41b4b06a7f135347f5958837e000
leipzigj <leipzigj@rescommap02.research.chop.edu>
2013-08-14 12:05:43 -0400

    json

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 nodeblast
 =========
-
 a node.js wrapper for bl2seq from BLAST (not BLAST+) so it can be offloaded to another server
+
+In theory this should be faster than a CGI wrapper.
+
+I use this for aligning stuff to the mitochondrial genome.
+
+
+
+Usage:
+`node nodeblast.js`
+
+http://rescommap02.research.chop.edu:8080/?defline=what&seq=TGGTCAACCTCGACCTAGGCCTCCTATTTATTCTAGCCACCG
+
---
+++ nodeblast.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var url = require('url');
+
+//load bl2seq config
+var config = require('./bl2seq.cfg.json').bl2seq;
+
+//this is a node.js wrapper for bl2seq
+//an 
+//boilerplate code from http://nodejs.org/
+http.createServer(function (req, res) {
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	var queryData = url.parse(req.url,true).query;
+	if(queryData.seq){
+	    if(!queryData.defline){
+		queryData.defline="sequence";
+	    }
+	    blastSeq(config,'>'+queryData.defline+'\\n'+queryData.seq+'\\n',res);
+	}else{
+	    res.end("No seq argument\n");
+	}
+}).listen(8080);
+
+//call bl2seq and print default format straight to browser
+function blastSeq(config,queryPath,browser){
+
+    //build the blast command and evaluate in bash to allow
+    //for process substitution so we are not creating temp files
+    var blastCmd="/bin/bash -c '/usr/local/bin/bl2seq";
+    for (var i = 0; i < config.length; i++) {
+	blastCmd += " "+config[i].argument+" "+config[i].value;
+    }
+    blastCmd += "'"
+	console.log(blastCmd);
+    //http://nodejs.org/api/child_process.html
+    var childProcess = require('child_process');
+    blast = childProcess.exec(blastCmd, function (error, stdout, stderr) {
+	if (error) {
+	    browser.write(error.stack);
+	    browser.write(stderr)
+	}else{
+	    browser.write(stdout);
+	}
+	browser.end();
+    });
+}
