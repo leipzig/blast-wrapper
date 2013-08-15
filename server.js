4d3ac1aff44a3f403887fb6084fd1163011a4f8a
Jeremy Leipzig <leipzig@gmail.com>
2013-08-15 10:52:02 -0400

    accepts POST; better validation

--- server.js
+++ server.js
@@ -1,47 +1,92 @@
 var http = require('http');
 var url = require('url');
-
+var qs = require('querystring');
 //load bl2seq config
 var baseDir = process.cwd();
 var config = require(baseDir + '/config.json');
 
 //this is a node.js wrapper for bl2seq
-
 //boilerplate code from http://nodejs.org/
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	var queryData = url.parse(req.url,true).query;
-	if(queryData.seq){
-	    if(!queryData.defline){
-		queryData.defline="sequence";
-	    }
-	    blastSeq(config,'>'+queryData.defline+'\\n'+queryData.seq+'\\n',res);
-	}else{
-	    res.end("No seq argument\n");
-	}
+//and http://stackoverflow.com/questions/4295782/how-do-you-extract-post-data-in-node-js
+http.createServer(function(req, res) {
+
+    var queryData = '';
+    if (req.method == 'POST') {
+        console.log("POST");
+        var body = '';
+        req.on('data', function(data) {
+            body += data;
+            if (body.length > 1e6) {
+                queryData = "";
+                res.writeHead(413, {
+                    'Content-Type': 'text/plain'
+                }).end();
+                req.connection.destroy();
+            }
+        });
+        req.on('end', function() {
+            queryData = qs.parse(body);
+            validate(queryData, res);
+        });
+    } else {
+        console.log("GET");
+        queryData = url.parse(req.url, true).query;
+        validate(queryData, res);
+    }
 }).listen(8080);
 
-//call bl2seq and print default format straight to browser
-function blastSeq(config,queryPath,browser){
+//the sequence is required, the name isn't
+
+function validate(queryData, res) {
+    if (queryData.seq) {
+        if (!queryData.name) {
+            queryData.name = "sequence";
+        }
+        var re = /^[A-Za-z]+$/;
+        if (queryData.seq.match(re)) {
+            blastSeq('>' + queryData.name + '\\n' + queryData.seq + '\\n', res);
+        } else {
+            contentError(res,"Invalid sequence\n");
+        }
+    } else {
+        contentError(res,"No seq argument\n");
+    }
+}
+
+function contentError(res,errorstr) {
+    res.writeHead(400, {
+        'Content-Type': 'text/plain'
+    });
+    res.end(errorstr);
+}
+
+//call blast and print default format straight to browser
+function blastSeq(queryPath, res) {
     //config.executable is the path to bl2seq
     //though it could be blastall with some tweaks
     //build the blast command and evaluate in bash to allow
     //for process substitution so we are not creating temp files
-    var blastCmd="/bin/bash -c '"+config.executable+" -i <(echo -e \""+queryPath+"\")";
+    var blastCmd = "/bin/bash -c '" + config.executable + " -i <(echo -e \"" + queryPath + "\")";
     for (var i = 0; i < config.params.length; i++) {
-	blastCmd += " "+config.params[i].argument+" "+config.params[i].value;
+        blastCmd += " " + config.params[i].argument + " " + config.params[i].value;
     }
     blastCmd += "'"
 
     //http://nodejs.org/api/child_process.html
     var childProcess = require('child_process');
-    blast = childProcess.exec(blastCmd, function (error, stdout, stderr) {
-	if (error) {
-	    browser.write(error.stack);
-	    browser.write(stderr)
-	}else{
-	    browser.write(stdout);
-	}
-	browser.end();
+    blast = childProcess.exec(blastCmd, function(error, stdout, stderr) {
+        if (error) {
+            contentError(error.stack);
+            res.writeHead(400, {
+                'Content-Type': 'text/plain'
+            });
+            res.write(error.stack);
+            res.end(stderr)
+        } else {
+            res.writeHead(200, {
+                'Content-Type': 'text/plain'
+            });
+            res.end(stdout);
+        }
     });
 }
