8c1dc0e723285495fd26f2786c99f068e69c24b6
Jeremy Leipzig <leipzig@gmail.com>
2018-01-21 15:49:53 -0500

    mocha

---
+++ test.js
@@ -0,0 +1,26 @@
+var assert = require('assert'),
+    http = require('http');
+
+describe('/', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:8080/?name=mysequence', function (res) {
+      assert.equal(400, res.statusCode);
+      done();
+    });
+  });
+
+  it('should say "No seq argument"', function (done) {
+    http.get('http://localhost:8080/?name=mysequence', function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function () {
+        assert.equal('No seq argument\n', data);
+        done();
+      });
+    });
+  });
+});
